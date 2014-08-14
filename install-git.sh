6b6b326edd1fafb9c0dd1fbe084095b7feed7303
Nico Kruger <nico.kruger@gmail.com>
2014-08-14 17:52:48 +0200

    Script to install git on sles sigh

---
+++ install-git.sh
@@ -0,0 +1,5 @@
+sudo zypper addrepo http://download.opensuse.org/repositories/devel:/languages:/perl/SLE_11_SP3/devel:languages:perl.repo
+sudo zypper addrepo http://download.opensuse.org/repositories/devel:/tools:/scm/SLE_11_SP2/devel:tools:scm.repo
+sudo zypper update
+sudo zypper install git-core
+
